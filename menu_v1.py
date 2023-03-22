50ca5abb0cc86e76faba4312aac3c99b4957358b
twschool <98859425+twschool@users.noreply.github.com>
2023-03-22 15:10:28 +1300

    Spellchecked and starting making dictionary

--- menu_v1.py
+++ menu_v1.py
@@ -1,6 +1,15 @@
-value_ = [["Beef buger", 5.69], ["Fries", 1.00], ["Fizzy drink"]]
+value_ = [["Beef burger", 5.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
 cheezy_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
 super_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Smoothies", 2.00]]
-menu = [value_, cheezy_, super_]
-for item in menu:
+
+# Print all food and prices off the value menu
+for item in value_:
+    print(f"Food: {item[1]}, Cost: {item[1]}")
+
+# Print all food and prices off the cheezy menu
+for item in cheezy_:
+    print(f"Food: {item[1]}, Cost: {item[1]}")
+
+# Print all food and prices off the super menu
+for item in super_:
     print(f"Food: {item[1]}, Cost: {item[1]}")
---
+++ menu_v2.py
@@ -0,0 +1,11 @@
+value_ = [["Beef burger", 5.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
+cheezy_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
+super_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Smoothies", 2.00]]
+menu = {
+    "Value" : {
+        "Beef burger" : 5.69, "Fries" : 1.00, "Fizzy drink" : 1.00
+        },
+        "Cheezy" : {
+            "C"}}
+for item in menu:
+    print(f"Food: {item[1]}, Cost: {item[1]}")
