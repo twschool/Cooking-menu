a60f56d98d24ce38bb64946c422263178e9eba20
twschool <98859425+twschool@users.noreply.github.com>
2023-03-22 15:11:30 +1300

    Formatted dictionary to be easily readable
    inspired by w3 schools website

--- menu_v2.py
+++ menu_v2.py
@@ -2,10 +2,21 @@ value_ = [["Beef burger", 5.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
 cheezy_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
 super_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Smoothies", 2.00]]
 menu = {
-    "Value" : {
-        "Beef burger" : 5.69, "Fries" : 1.00, "Fizzy drink" : 1.00
-        },
-        "Cheezy" : {
-            "C"}}
+    "Value": {
+        "Beef burger": 5.69,
+        "Fries": 1.00,
+        "Fizzy drink": 1.00
+    },
+    "Cheezy": {
+        "Cheeseburger": 6.69,
+        "Fries": 1.00,
+        "Fizzy drink": 1.00
+    },
+    "Super": {
+        "Cheeseburger": 6.69,
+        "Fries": 1.00,
+        "Smoothies": 2.00
+    }
+}
 for item in menu:
     print(f"Food: {item[1]}, Cost: {item[1]}")
