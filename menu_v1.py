337559820f12fcf8fc560c74674a6511cf843a9e
twschool <98859425+twschool@users.noreply.github.com>
2023-03-22 17:14:52 +1300

    Brief fix
    Changed v1 so it would print out the data similar to v2

--- menu_v1.py
+++ menu_v1.py
@@ -3,13 +3,18 @@ cheezy_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
 super_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Smoothies", 2.00]]
 
 # Print all food and prices off the value menu
+print("Value: ")
 for item in value_:
     print(f"Food: {item[1]}, Cost: {item[1]}")
+print()
 
 # Print all food and prices off the cheezy menu
+print("Cheezy: ")
 for item in cheezy_:
     print(f"Food: {item[1]}, Cost: {item[1]}")
+print()
 
 # Print all food and prices off the super menu
+print("Super")
 for item in super_:
     print(f"Food: {item[1]}, Cost: {item[1]}")
