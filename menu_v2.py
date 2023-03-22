fb63bcd85bfaf9d7a696f3a466eb9619f71ba11b
twschool <98859425+twschool@users.noreply.github.com>
2023-03-22 15:18:27 +1300

    Removed the lists for reference and added a for loop to iterate through all of the stuff

--- menu_v2.py
+++ menu_v2.py
@@ -1,6 +1,3 @@
-value_ = [["Beef burger", 5.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
-cheezy_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Fizzy drink", 1.00]]
-super_ = [["Cheeseburger", 6.69], ["Fries", 1.00], ["Smoothies", 2.00]]
 menu = {
     "Value": {
         "Beef burger": 5.69,
@@ -18,5 +15,8 @@ menu = {
         "Smoothies": 2.00
     }
 }
-for item in menu:
-    print(f"Food: {item[1]}, Cost: {item[1]}")
+for menu_name, menu_items in menu.items():
+    print(f"{menu_name}:")
+    for food, price in menu_items.items():
+        print(f"{food}: {price}")
+    print()
