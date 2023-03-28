0f5887a61527253f3b187ebfeb93b27a4ed289e8
twschool <98859425+twschool@users.noreply.github.com>
2023-03-28 22:08:39 +1300

    Edit data works
    The code works!!! pain and suffering has gone into this code version 2 is version 2morrow

--- edit_data_v1.py
+++ edit_data_v1.py
@@ -26,6 +26,8 @@ for combo_name in menu:
     all_combos = f"{all_combos}\n{combo_name}"
 
 while True:
+    enterbox_values = []
+    enterbox_fields = []
     # Get the user input on what combo they would like to edit
     try:
         edit_combo = eg.enterbox(f"{error}All combo names: {all_combos}\nWhat combo would you like to edit?").title()
@@ -40,14 +42,21 @@ while True:
         continue
     
     enterbox_values = [edit_combo]
-    enterbox_field = []
-    enterbox_values
-    
+    enterbox_fields = []
+    print(dict_edit)
     for item in dict_edit.items():
         """Add the existing values to the enterbox so the values
         are filled in automatically and can be easily edited"""
-        enterbox_values.append(item)
-        enterbox_values.append(dict_edit[item])
+        enterbox_values.append(item[0])
+        enterbox_values.append(item[1])
+        # Add the enterbox fields in
+        current_index = list(dict_edit.items()).index(item)
+        enterbox_fields.append(f"Item {current_index}")
+        enterbox_fields.append(f"Item {current_index} price")
+    
 
-
-
+    output = eg.multenterbox(msg=f"Here is the values of the {edit_combo} combo", fields=enterbox_fields, values=enterbox_values)
+    if output == dict_edit:
+        print("Same")
+    print(f"Output: {output}")
+    error = ""
